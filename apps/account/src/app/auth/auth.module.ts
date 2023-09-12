580ce5ff825146e8dfdae2e24c53bf16a664758c
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-12 19:55:02 -0400

    congigured auth logic

--- apps/account/src/app/auth/auth.module.ts
+++ apps/account/src/app/auth/auth.module.ts
@@ -1,8 +1,13 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+
 import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
+import { getJwtConfig } from '../configs/jwt.config';
 
 @Module({
+  imports: [UserModule, JwtModule.registerAsync(getJwtConfig())],
   controllers: [AuthController],
   providers: [AuthService],
 })
