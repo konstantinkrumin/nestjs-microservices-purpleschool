2ff24cee70fe8044a380a37c4244a004f512c7fa
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-18 13:48:19 -0400

    adde contracts for login and register

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AccountLogin, AccountRegister } from '@purpleschool/contracts';
 
 export class RegisterDto {
   email: string;
@@ -7,22 +8,21 @@ export class RegisterDto {
   displayName?: string;
 }
 
-export class LoginDto {
-  email: string;
-  password: string;
-}
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() dto: RegisterDto) {
+  async register(
+    @Body() dto: AccountRegister.Request
+  ): Promise<AccountRegister.Response> {
     return this.authService.register(dto);
   }
 
   @Post('login')
-  async login(@Body() { email, password }: LoginDto) {
+  async login(
+    @Body() { email, password }: AccountLogin.Request
+  ): Promise<AccountLogin.Response> {
     const { id } = await this.authService.validateUser(email, password);
     return this.authService.login(id);
   }
