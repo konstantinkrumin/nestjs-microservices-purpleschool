9f8ac55858a5809b85db3c053ed5624317fd59e8
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-27 17:44:36 -0400

    fixed coourseid bug

--- apps/account/src/app/user/entities/user.entity.ts
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -27,7 +27,7 @@ export class UserEntity implements IUser {
   }
 
   public setCourseStatus(courseId: string, state: PurchaseState) {
-    const exist = this.courses.find((c) => c._id === courseId);
+    const exist = this.courses.find((c) => c.courseId === courseId);
 
     if (!exist) {
       this.courses.push({
@@ -38,12 +38,12 @@ export class UserEntity implements IUser {
     }
 
     if (state === PurchaseState.Canceled) {
-      this.courses = this.courses.filter((c) => c._id !== courseId);
+      this.courses = this.courses.filter((c) => c.courseId !== courseId);
       return this;
     }
 
     this.courses = this.courses.map((c) => {
-      if (c._id === courseId) {
+      if (c.courseId === courseId) {
         c.purchaseState === state;
         return c;
       }
